f81f0b029ac2e18911e14d06eb3b915c7a7a3131
PRIYANKA-Nigam <nigampriyanka042@gmail.com>
2022-11-03 15:53:43 +0530

    8th commit

---
+++ app/src/main/java/com/example/shlokmantraapp/DarkModeActivity.java
@@ -0,0 +1,14 @@
+package com.example.shlokmantraapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DarkModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dark_mode);
+    }
+}
